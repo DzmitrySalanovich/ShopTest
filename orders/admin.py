bfa9d07ac0cecaf320eba02a3ede5ebc83be0bd3
Dzmitry Salanovich <Dzmitry.Salanovich@itechart-group.com>
2019-07-22 16:27:39 +0300

    fix admins.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from orders.models import Order, LineItem
 
+@admin.register(LineItem)
 class OrderAdmin(admin.ModelAdmin):
     pass  
 
+@admin.register(Order)
 class LineItemAdmin(admin.ModelAdmin):
     pass
-
-admin.site.register(LineItem, LineItemAdmin)
-admin.site.register(Order, OrderAdmin)
--- products/admin.py
+++ products/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from products.models import Category, Product, Product_att
 
+@admin.register(Product_att)
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Category)
 class ProductAdmin(admin.ModelAdmin):
     pass
 
+@admin.register(Product)
 class Product_attAdmin(admin.ModelAdmin):
     pass
-    
-admin.site.register(Product_att, Product_attAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Product, ProductAdmin)
