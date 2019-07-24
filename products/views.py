d864c15cc2ec53b6ac09ba1bec99b0d741bdb3d9
Dzmitry Salanovich <Dzmitry.Salanovich@itechart-group.com>
2019-07-24 17:56:39 +0300

    fix product app

--- products/views.py
+++ products/views.py
@@ -11,12 +11,25 @@ from django.shortcuts import render
 from .models import Category, Product, Product_att
 from .serializers import CategorySerializer, ProductSerializer, Product_attSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import FilterSet
+from django_filters import filters
+
+
+class ProductFilter(FilterSet):
+    min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        fields = ['min_price', 'max_price']
+
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['price']
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = ProductFilter
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -24,5 +37,5 @@ class CategoryViewSet(viewsets.ModelViewSet):
         
     @action(detail=True, methods=['get'])
     def subcategories(self, request, *args, **kwargs):
-        self.queryset = self.get_object().subcategories
+        self.queryset = self.get_object().subcategories.all()
         return self.list(request)
